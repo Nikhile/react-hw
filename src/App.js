b5aced5888ac6134bfba7e604ec57e13d86f5f2f
NIK <nikhilelpula@gmail.com>
2021-07-21 19:33:33 -0500

    React-h/w: initial commit

---
+++ src/App.js
@@ -0,0 +1,212 @@
+import React, { useState, useEffect } from "react";
+import fetch from './mockData/mockCustomerData';
+import { useTable } from 'react-table'
+import "./App.css";
+import _ from 'lodash';
+
+function calculateRewardPoints(customerData) {
+  const months = [
+    "Jan", "Feb", "Mar", "Apr", 
+    "May", "Jun", "Jul", "Aug", 
+    "Sep", "Oct", "Nov", "Dec"];
+  const transactionPoints = customerData.map(transaction => {
+    let points = 0;
+    let over100 = transaction.amountSpent - 100;
+
+    if (over100 > 0) {
+      points += (over100 * 2);
+    }
+
+    if (transaction.amountSpent > 50) {
+      points += 50;
+    }
+
+    const month = new Date(transaction.transctionDate).getMonth();
+    return { ...transaction, points, month };
+  });
+
+  let byCustomer = {};
+  let totalPointsByCustomer = {};
+
+  transactionPoints.forEach(transactionPoints => {
+    let { customerId, customerName, month, points } = transactionPoints;
+
+    if (!byCustomer[customerId]) {
+      byCustomer[customerId] = [];
+    }
+
+    if (!totalPointsByCustomer[customerId]) {
+      totalPointsByCustomer[customerName] = 0;
+    }
+console.log(totalPointsByCustomer[customerName], points);
+    totalPointsByCustomer[customerName] += points;
+
+    if (byCustomer[customerId][month]) {
+      byCustomer[customerId][month].points += points;
+      byCustomer[customerId][month].monthNumber = month;
+      byCustomer[customerId][month].numTransactions++;
+    }
+    else {
+      byCustomer[customerId][month] = {
+        customerId,
+        customerName,
+        monthNumber: month,
+        month: months[month],
+        numTransactions: 1,
+        points
+      }
+    }
+  });
+
+  let total = [];
+  for (var custKey in byCustomer) {
+    byCustomer[custKey].forEach(cRow => {
+      total.push(cRow);
+    });
+  }
+
+  let totalCustomerSpent = [];
+  for (custKey in totalPointsByCustomer) {
+    totalCustomerSpent.push({
+      customerName: custKey,
+      points: totalPointsByCustomer[custKey]
+    });
+  }
+
+  return {
+    summaryByCustomer: total,
+    transactionPoints,
+    totalPointsByCustomer: totalCustomerSpent
+  };
+}
+
+const columns = [
+  {
+    Header: 'Customer Name',
+    accessor: 'customerName'
+  },
+  {
+    Header: 'Transaction Month',
+    accessor: 'month'
+  },
+  {
+    Header: "No of Transactions",
+    accessor: 'numTransactions'
+  },
+  {
+    Header: 'Reward Points',
+    accessor: 'points'
+  }
+];
+
+const pointsColumn = [
+  {
+    Header: 'Customer Name',
+    accessor: 'customerName'
+  },
+  {
+    Header: 'Total Reward Points',
+    accessor: 'points'
+  }
+];
+
+function App() {
+  const [transactionData, setTransactionData] = useState([]);
+  const [customrData, setCustomerData] = useState([]);
+  const firstTable = useTable({
+    columns,
+    data: transactionData,
+  })
+
+  const secondTable = useTable({
+    columns: pointsColumn,
+    data: customrData,
+  })
+
+  useEffect(() => {
+    fetch().then((data) => {
+      const results = calculateRewardPoints(data);
+      setTransactionData(results.summaryByCustomer);
+      setCustomerData(results.totalPointsByCustomer)
+    });
+  }, []);
+
+  if (transactionData == null) {
+    return <div>Empty Data</div>;
+  }
+
+  return (<div>
+    <div className="container">
+      <div className="row">
+        <div className="col-10">
+          <h2 className="Header">Customer Total points by Month</h2>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-8">
+          <table {...firstTable.getTableProps()}>
+            <thead>
+              {firstTable.headerGroups.map(headerGroup => (
+                <tr {...headerGroup.getHeaderGroupProps()}>
+                  {headerGroup.headers.map(column => (
+                    <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                  ))}
+                </tr>
+              ))}
+            </thead>
+            <tbody {...firstTable.getTableBodyProps()}>
+              {firstTable.rows.map((row, i) => {
+                firstTable.prepareRow(row)
+                return (
+                  <tr {...row.getRowProps()}>
+                    {row.cells.map(cell => {
+                      return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                    })}
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+
+    <div className="container">
+      <div className="row">
+        <div className="col-10">
+          <h2>Total Points Earned By Customer</h2>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-8">
+          <table {...secondTable.getTableProps()}>
+            <thead>
+              {secondTable.headerGroups.map(headerGroup => (
+                <tr {...headerGroup.getHeaderGroupProps()}>
+                  {headerGroup.headers.map(column => (
+                    <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                  ))}
+                </tr>
+              ))}
+            </thead>
+            <tbody {...secondTable.getTableBodyProps()}>
+              {secondTable.rows.map((row, i) => {
+                secondTable.prepareRow(row)
+                return (
+                  <tr {...row.getRowProps()}>
+                    {row.cells.map(cell => {
+                      return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                    })}
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+  );
+}
+
+export default App;
---
+++ src/mockData/mockCustomerData.js
@@ -0,0 +1,114 @@
+export default function() {
+    return Promise.resolve(
+      [
+          {
+            customerId: 1,
+            customerName: "Elon",
+            amountSpent: 125,
+            transctionDate: "06-01-2020"
+          },
+          {
+            customerId: 1,
+            customerName: "Elon",
+            amountSpent: 125,
+            transctionDate: "07-21-2020"
+          },
+          {
+            customerId: 1,
+            customerName: "Elon",
+            amountSpent: 125,
+            transctionDate: "08-21-2020"
+          },
+          {
+            customerId: 1,
+            customerName: "Elon",
+            amountSpent: 125,
+            transctionDate: "08-25-2020"
+          },
+          {
+            customerId: 1,
+            customerName: "Elon",
+            amountSpent: 125,
+            transctionDate: "09-21-2020"
+          },
+          {
+            customerId: 1,
+            customerName: "Elon",
+            amountSpent: 125,
+            transctionDate: "09-27-2020"
+          },
+          {
+            customerId: 1,
+            customerName: "Elon",
+            amountSpent: 125,
+            transctionDate: "11-04-2020"
+          },
+          {
+            customerId: 1,
+            customerName: "Elon",
+            amountSpent: 125,
+            transctionDate: "11-06-2020"
+          },
+          {
+            customerId: 1,
+            customerName: "Elon",
+            amountSpent: 125,
+            transctionDate: "11-21-2020"
+          },
+          {
+            customerId: 2,
+            customerName: "Jeff",
+            amountSpent: 125,
+            transctionDate: "02-01-2020"
+          },
+          {
+            customerId: 2,
+            customerName: "Jeff",
+            amountSpent: 75,
+            transctionDate: "05-21-2020"
+          },
+          {
+            customerId: 2,
+            customerName: "Jeff",
+            amountSpent: 10,
+            transctionDate: "06-01-2020"
+          },
+          {
+            customerId: 2,
+            customerName: "Jeff",
+            amountSpent: 75,
+            transctionDate: "06-21-2020"
+          },
+          {
+            customerId: 2,
+            customerName: "Jeff",
+            amountSpent: 200,
+            transctionDate: "07-01-2020"
+          },
+          {
+            customerId: 2,
+            customerName: "Jeff",
+            amountSpent: 224,
+            transctionDate: "07-21-2020"
+          },
+          {
+            customerId: 3,
+            customerName: "Richard",
+            amountSpent: 120,
+            transctionDate: "03-21-2020"
+          },
+          {
+            customerId: 3,
+            customerName: "Richard",
+            amountSpent: 120,
+            transctionDate: "04-22-2020"
+          },
+          {
+            customerId: 3,
+            customerName: "Richard",
+            amountSpent: 320,
+            transctionDate: "05-22-2020"
+          }
+      ]
+    );
+  };
